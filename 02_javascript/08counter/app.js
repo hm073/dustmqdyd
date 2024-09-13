97c6d08630988f4b12daf48e08b6401ac9071b05
hm073 <syh649@gmail.com>
2024-09-13 17:54:15 +0900

    up

--- 02_javascript/08counter/app.js
+++ 02_javascript/08counter/app.js
@@ -1,1 +1,12 @@
-document.addEventListener("DOMContentLoaded")
+document.addEventListener("DOMContentLoaded", () => {
+    let cont = 0;
+
+    const value = document.querySelector("#value");
+    const btns = document.querySelectorAll(".btn");
+
+    btns.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            
+        })
+    })
+}); //end
