a63888776e011fe95a1a92888fa0cc0f8db9d0ec
hm073 <syh649@gmail.com>
2024-10-02 17:58:55 +0900

    u

--- 10월/1001/1001-2 side에서 나오는 menu/js/chania.js
+++ 10월/1001/1001-2 side에서 나오는 menu/js/chania.js
@@ -39,7 +39,16 @@ document.addEventListener("DOMContentLoaded", () => {
 		i.addEventListener("click", () => {
 			menubtn.textContent = "dehaze";
 			nav.style.left = "-110%";
-		})
-	})
+		});
+	});
+
+	//문서의 다른 부분을 클릭했을 때 nav를 닫는 이벤트
+	document.addEventListener("click", (event) => {
+		if(!menubtn.contains(event.target) && !nav.contains(event.target)) {
+			menubtn.textContent = "dehaze";
+			nav.style.left = "-110%";
+		}
+		//클릭한 요소가 menubtn이나 nav안에 포함되어 있지 않으면(두 조건을 모두 충족할 경우) nav를 닫는다(-110%)
+	});
 	
 })//end
