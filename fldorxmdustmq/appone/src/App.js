c050dcf3c0ce05016c9d71792f8d371bda2d8676
hm073 <syh649@gmail.com>
2024-10-25 17:39:51 +0900

    1025

--- fldorxmdustmq/appone/src/App.js
+++ fldorxmdustmq/appone/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useState} from 'react';
 
 function Header(props){
   // console.log('props',props, props.title)
@@ -41,17 +42,27 @@ function Article(i){
 }
 
 function App() {
+  const _mode = useState('WELCOME');
+  const mode = _mode[0];
+  const setMode = mode[1];
   const topics = [
     {id:1, title:'html', body:'html is...'},
     {id:2, title:'css', body:'css is...'},
     {id:3, title:'javascript', body:'javascript is...'}
   ]
+
+  let content = null;
+  if(mode === 'WELCOME') {
+    content = <Article a='Welcome' b='Hello, WEB'></Article>
+  } else if(mode === 'READ') {
+    content = <Article a='Read' b='Hello, Read'></Article>
+  }
   
   return (
     <div>
       <Header title='REACT' onChangeMode={() => {alert('Header')}}></Header>
       <Nav topics={topics} onChangeMode={(id)=>{alert(id);}}></Nav>
-      <Article a='Welcome' b='Hello, world'></Article>
+      {content}
     </div>
   );
 }
---
+++ fldorxmdustmq/apptwo/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="con">
+      
+    </div>
+  );
+}
+
+export default App;
